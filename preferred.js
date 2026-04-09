89239075186da10dab028fb0853438e12de00a25
A-D-I-T-Y-A-K-U-M-A-R <adityakrsingh08b04@gmail.com>
2026-04-09 17:53:39 +0530

    Update preferred.js

--- preferred.js
+++ preferred.js
@@ -56,7 +56,7 @@ let n = inst.toLowerCase();
 
 if(n.includes("indian institute of technology")) return "IIT";
 if(n.includes("national institute of technology")) return "NIT";
-if(n.includes("iiit")) return "IIIT";
+if(n.includes("indian institute of information technology")) return "IIIT";
 if(n.includes("birla institute of technology")) return "BIT";
 
 return "OTHER";
